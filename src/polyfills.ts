c41c2617efd3e132e8a8524f5ca5bfed74bb30bd
Kirill <krboktv@gmail.com>
2020-06-22 00:17:44 +0300

    fix build

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,7 +56,6 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'process';
 
 (window as any).global = window;
 /***************************************************************************************************
