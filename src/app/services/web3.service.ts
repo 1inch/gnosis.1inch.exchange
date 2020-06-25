bb6a56adabb9051c0be9a571cd2e251a764b7029
Kirill <krboktv@gmail.com>
2020-06-25 17:30:38 +0300

    add 1inch web3 provider

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -7,57 +7,74 @@ import Web3 from 'web3';
 const w = window as any;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class Web3Service {
 
-  web3Subject$ = new Subject<Web3>();
-  web3$ = this.web3Subject$.asObservable().pipe(
-    shareReplay({ bufferSize: 1, refCount: true })
-  );
+    public rpcUrl = 'https://ethereum.1inch.exchange/';
 
-  constructor() {
-    // need to subscribe first
-    this.web3$.subscribe();
-    this.activate();
+    private authPart1 = 'd2ViMzpzbGRna2psc2RrajIzOTg0NzV1b3doc2pkbmdsc25na2xqaHNka2d6bzM0';
+    private authPart2 = 'dXpoanRsbnNkbGduc2RsZ2pvc2R1em93NG5sZ3NuZGdrbGpoc2R1Z3p1aW93bmV0';
+    private authPart3 = 'Z3NqZG5nb3V6c2RvZ2pubHNkaGpnb3Nq';
 
-    (window as any)?.ethereum?.on('accountsChanged', () => this.activate());
-  }
-
-  public activate(): void {
-    this.activateWeb3().then(
-      (w3) => this.web3Subject$.next(w3),
-      (e) => console.error(e)
+    private web3Subject$ = new Subject<Web3>();
+    web3$ = this.web3Subject$.asObservable().pipe(
+        shareReplay({ bufferSize: 1, refCount: true })
     );
-  }
 
-  public async activateWeb3(): Promise<Web3> {
-    const web3 = new Web3(w.ethereum);
-    if (!web3.defaultAccount) {
-      await w.ethereum.enable();
+    constructor() {
+        // need to subscribe first
+        this.web3$.subscribe();
+        this.activate();
+
+        // (window as any)?.ethereum?.on('accountsChanged', () => this.activate());
     }
-    return web3;
-  }
 
-  public getInstance(abi: any[], address: string): Observable<Contract> {
+    public activate(): void {
+        this.activateWeb3().then(
+            (w3) => this.web3Subject$.next(w3),
+            (e) => console.error(e)
+        );
+    }
 
-    return this.web3$.pipe(
-      map((web3) => {
+    public async activateWeb3(): Promise<Web3> {
+        // const web3 = new Web3(w.ethereum);
+        // if (!web3.defaultAccount) {
+        //     await w.ethereum.enable();
+        // }
+        // return web3;
+        const opt = {
+            withCredentials: true,
+            headers: [
+                {
+                    name: 'Authorization',
+                    value: 'Basic ' + this.authPart1 + this.authPart2 + this.authPart3
+                }
+            ]
+        };
+        const provider = new Web3.providers.HttpProvider(this.rpcUrl, opt);
+        return new Web3(provider);
+    }
+
+    public getInstance(abi: any[], address: string): Observable<Contract> {
+
+        return this.web3$.pipe(
+            map((web3) => {
+                // @ts-ignore
+                return (new web3.eth.Contract(
+                    abi,
+                    address
+                )) as Contract;
+            }),
+            // catchError( () => { }) ???
+        );
+    }
+
+    public getInstanceWithoutNetwork(abi: any[]): Contract {
+
+        const web3 = new Web3('');
+
         // @ts-ignore
-        return (new web3.eth.Contract(
-          abi,
-          address
-        )) as Contract;
-      }),
-      // catchError( () => { }) ???
-    );
-  }
-
-  public getInstanceWithoutNetwork(abi: any[]): Contract {
-
-    const web3 = new Web3('');
-
-    // @ts-ignore
-    return new web3.eth.Contract(abi) as Contract;
-  }
+        return new web3.eth.Contract(abi) as Contract;
+    }
 }
