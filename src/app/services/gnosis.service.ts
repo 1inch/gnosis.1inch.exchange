0d6bc762f03b9b50da2920a6cf3b07ccd904711a
Kirill <krboktv@gmail.com>
2020-06-29 17:18:07 +0300

    fix

--- src/app/services/gnosis.service.ts
+++ src/app/services/gnosis.service.ts
@@ -38,14 +38,14 @@ export class GnosisService {
 
     public addListeners(): void {
 
-        this.walletAddress.next('0x3a13D9b322F391a1AFab36a1d242C24F3250bA48');
-        // appsSdk.addListeners({
-        //     onSafeInfo: ((info: SafeInfo) => {
-        //
-        //         this.isMainNet.next(info.network.toLowerCase() === 'mainnet');
-        //         this.walletAddress.next(info.safeAddress);
-        //     })
-        // });
+        // this.walletAddress.next('0x3a13D9b322F391a1AFab36a1d242C24F3250bA48');
+        appsSdk.addListeners({
+            onSafeInfo: ((info: SafeInfo) => {
+
+                this.isMainNet.next(info.network.toLowerCase() === 'mainnet');
+                this.walletAddress.next(info.safeAddress);
+            })
+        });
     }
 
     public removeListeners(): void {
